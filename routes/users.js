188a8b7aa33a723bad78c4d8ffa108ed7c391b7e
Justin Elliott <thecrashtestjustin@gmail.com>
2023-12-19 07:48:16 -0800

    setting up routes for user registration

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,13 @@ const passport = require("passport");
 const User = require("../model/user");
 const bcrypt = require("bcryptjs");
 
+const user_controller = require("../controllers/userController");
+
 //GET Sign Up Form
-router.get("/sign-up", function (req, res, next) {
-  res.render("sign-up-form");
-});
+router.get("/sign-up", user_controller.user_register_get);
+//POST Sign up Form
+router.post("/sign-up", user_controller.user_register_post);
+//GET Registration Password Page
+router.get("/registration-password", user_controller.user_register_password);
 
 module.exports = router;
