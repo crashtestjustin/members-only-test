2b02989088f0bc003dd6b2bf51e6e6d378318733
Justin Elliott <thecrashtestjustin@gmail.com>
2024-01-02 06:57:09 -0800

    added a delete message route

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,6 @@ const message_controller = require("../controllers/messageController");
 
 router.get("/", message_controller.index);
 
+router.post("/delete-message", message_controller.delete_messsage_post);
+
 module.exports = router;
