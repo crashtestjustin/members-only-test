f3793d0cc2484fd516ec648ddfb820e08bf87a70
Justin Elliott <thecrashtestjustin@gmail.com>
2024-01-04 07:55:08 -0800

    added GET and POST controllers for creating new messages

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,5 +1,6 @@
 const Message = require("../model/messages");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.index = asyncHandler(async (req, res, next) => {
   const messages = await Message.find().populate("user").exec();
@@ -29,3 +30,49 @@ exports.delete_messsage_confirm_post = asyncHandler(async (req, res, next) => {
   console.log(`DELETED ${message}`);
   res.redirect("/");
 });
+
+exports.new_message_get = asyncHandler(async (req, res, next) => {
+  if (!req.user || !req.user._id) {
+    res.redirect("/log-in");
+    return;
+  }
+
+  res.render("new-message", {
+    title: "New Message",
+    user: req.isAuthenticated() ? req.user : null,
+  });
+});
+
+exports.new_message_post = [
+  body("message", "There must be a message to submit")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      console.log(errors.array());
+      return res.render("new-message", {
+        title: "New Message",
+        user: req.isAuthenticated() ? req.user : null,
+        errors: errors.array(),
+      });
+    }
+
+    if (!req.user || !req.user._id) {
+      res.redirect("/log-in");
+      return;
+    }
+
+    const message = new Message({
+      text: req.body.message,
+      posted: new Date(),
+      user: req.user._id,
+    });
+
+    await message.save();
+    res.redirect("/");
+  }),
+];
